5965c52f381a6b19a5a95ad54047cf5bc3674369
annabadritdinova <anna.badritdinova@gmail.com>
2021-06-21 22:47:18 -0400

    updt

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Contacts = require('../../model/index')
-const { validationCreateContact, validationUpdateContact } = require('./validation')
+const { validationIdContact, validationCreateContact, validationUpdateContact, validationFavoriteContact } = require('./validation')
 
 router.get('/', async (req, res, next) => {
   try {
@@ -13,7 +13,7 @@ router.get('/', async (req, res, next) => {
   
 })
 
-router.get('/:contactId', async (req, res, next) => {
+router.get('/:contactId', validationIdContact, async (req, res, next) => {
   try {
     const contact = await Contacts.getContactById(req.params.contactId)
     if (contact) {
@@ -87,5 +87,27 @@ router.put('/:contactId', validationUpdateContact, async (req, res, next) => {
     next(e)
   }
 })
+
+router.patch('/:contactId/favorite', validationFavoriteContact, async (req, res, next) => {
+  try {
+    const contact = await Contacts.updateContact(
+      req.params.contactId, req.body)
+    if (contact) {
+      return res.json({
+        status: 'success',
+        code: 200,
+        data: { contact },
+      })
+    } else {
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        data: 'Not Found',
+      })
+    }
+  } catch (e) {
+    next(e)
+  }
+});
 
 module.exports = router
